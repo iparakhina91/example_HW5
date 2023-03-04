f35f1e32948b337c6a6a395be38549e58eebf240
Irina <iparakhina91@gmail.com>
2023-03-04 03:02:22 +0300

    deleted import

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
-import static java.nio.channels.Selector.open;
 
 public class DragAndDropTest {
     @BeforeAll
